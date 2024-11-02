2ad43c3788dfb5be43b8e5e86c4e88ab20cb2729
vandriodd <ludasaadi@gmail.com>
2024-11-02 13:38:29 -0300

    feat(login): add basic login UI (without validation)

--- src/app/users/login/page.tsx
+++ src/app/users/login/page.tsx
@@ -1,9 +1,70 @@
-const LoginPage = () => {
+'use client'
+
+import Image from 'next/image'
+import { useState } from 'react'
+import { EyeIcon, EyeOffIcon } from '@/app/assets'
+
+export default function LoginPage() {
+  const [visible, setVisible] = useState(false)
+
   return (
-    <main>
-      <h1 className='text-4xl font-bold'>Login</h1>
+    <main className='flex flex-col'>
+      <Image
+        className='mb-10'
+        src='/logo.svg'
+        width={100}
+        height={100}
+        alt='Willinn logo'
+      />
+      <div className='flex w-[550px] flex-col items-center justify-center rounded-xl bg-white p-10'>
+        <h1 className='text-2xl font-medium'>Inicia sesión</h1>
+        <form className='mt-8 flex w-full flex-col gap-6'>
+          <div className='space-y-2'>
+            <label className='pl-1 font-medium'>E-mail</label>
+            <input
+              type='email'
+              className='w-full rounded-lg border border-[#E8E9EA] bg-white p-4 placeholder-[#949CA9] focus:border-accent focus:outline-none'
+              placeholder='Introduce tu mail'
+              required
+            />
+          </div>
+
+          <div className='space-y-2'>
+            <label className='pl-1 font-medium'>Contraseña</label>
+            <div className='relative flex'>
+              <input
+                type={visible ? 'text' : 'password'}
+                className='w-full rounded-lg border border-[#E8E9EA] bg-white p-4 placeholder-[#949CA9] focus:border-accent focus:outline-none'
+                placeholder='Introduce tu contraseña'
+                required
+              />
+              <button
+                className='absolute inset-y-0 right-0 pr-4 focus:outline-none'
+                type='button'
+                onClick={() => {
+                  setVisible(!visible)
+                }}
+              >
+                {visible ? <EyeOffIcon /> : <EyeIcon />}
+              </button>
+            </div>
+          </div>
+
+          <button
+            type='submit'
+            className='w-full rounded-lg bg-accent py-4 font-medium text-white hover:bg-[#da2381]'
+          >
+            Ingresar
+          </button>
+
+          <a
+            className='flex justify-end text-right text-[#263A66] hover:text-accent'
+            href='#'
+          >
+            ¿Olvidaste tu contraseña?
+          </a>
+        </form>
+      </div>
     </main>
   )
 }
-
-export default LoginPage
